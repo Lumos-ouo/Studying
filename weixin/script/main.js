1f6e291c4a4e4e1e78bb449117dc8f64205dd633
Zjuxhj <163619478+Zjuxhj@users.noreply.github.com>
2024-11-15 22:03:28 +0800

    Add files via upload

---
+++ weixin/script/main.js
@@ -0,0 +1,7 @@
+let myHeading=document.querySelector("h1");
+myHeading.textContent="Hello world!";
+myHeading.style.color="red";
+myHeading.style.backgroundColor="yellow";
+myHeading.style.fontSize="50px";
+myHeading.style.textAlign="center";
+myHeading.style.fontStyle="italic";
